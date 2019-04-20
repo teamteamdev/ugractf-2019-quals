4df02d39237540789a43a7ecba9a7c259aa8226a
Nikita Sychev <root@nsychev.ru>
2019-04-20 03:08:10 +0300

    add task thebook

---
+++ tasks/thebook/app/app.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+
+from datetime import datetime, timedelta
+from flask import Flask, request, redirect
+
+app = Flask(__name__)
+
+SECRET_PATH = '/Cb2F87Ed60a4d290E2DdB7ffDB77a2e5'
+SECRET_RESPONSE = 'ugra_ea5y_f4st_brutef0rce'
+
+REDIRECT_LINK = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return "404", 404
+
+
+@app.route(SECRET_PATH)
+def get_flag():
+    return SECRET_RESPONSE
+
+
+@app.route('/')
+def index():
+    return app.send_static_file('book.html')
+
+
+@app.route('/<unused>')
+def rickroll(unused):
+    return f'''<meta http-equiv="refresh" content="0;url='{REDIRECT_LINK}'" /><h1>Redirecting...</h1>'''
+
+
+if __name__ == "__main__":
+    app.run()
+
